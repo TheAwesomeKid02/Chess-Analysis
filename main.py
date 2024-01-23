87742836c9cf484dff1e6e24269c5c6551ffc57a
TheAwesomeKid02 <69116138+TheAwesomeKid02@users.noreply.github.com>
2024-01-23 16:25:55 +0000

    Made an analyzer page

--- main.py
+++ main.py
@@ -11,10 +11,14 @@ app = Flask(__name__, template_folder='front_end', static_folder='static')
 def index():
 	return render_template('index.html')
 
+@app.route('/analyze', methods=["GET"])
+def analyzer_page():
+	return render_template('analyze')
+
 @app.route('/', methods=["POST"])
 def index_post():
 	FEN = request.form['FEN']
-	return render_template('index.html', fen=FEN)
+	return render_template('analyzer_page.html', fen=FEN)
 
 if __name__ == '__main__':
 		# Run the Flask app
