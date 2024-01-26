65bb706037253ed771e87aaa6921d587a7a07d93
TheAwesomeKid02 <69116138+TheAwesomeKid02@users.noreply.github.com>
2024-01-26 02:04:55 +0000

    updated the analysis and started to broadcast messages

--- analysis.py
+++ analysis.py
@@ -1,151 +1,131 @@
-FENnotation = input('Enter code in FEN or Forsyth Edward Notation(please only include first 8 strings): ')
-turn = input("Who's turn is it? Type w if White and type b if Black?: ")
-print(FENnotation)
-print(turn)
+FEN = 
+turn = 
 FENCHANGE = []
 PAWNS = [0] * 64
 KNIGHTS = [0] * 64
 BISHOPS = [0] * 64
-ROOKS = [0] * 64
+ROOKS = [0] * 64 
 KINGS = [0] * 64
 QUEENS = [0] * 64
 UNIONNOK = [0] * 64
 UNIONK = [0] * 64
 legalMoves = []
-def conversion():
-	for char in FENnotation:
-		FENCHANGE.append(char)
-	
-	for index, item in enumerate(FENCHANGE):
-		while tempvar > 0:
-			if tempvar > 0:
-				tempvar = tempvar-1
-			if item == 'r' and tempvar <= 0:
-					ROOKS[index] = 5
-					UNIONNOK[index] = 5
-					UNIONK[index] = 5
-			elif item == 'R' and tempvar <= 0:
-					ROOKS[index] = -5
-					UNIONNOK[index] = -5
-					UNIONK[index] = -5
-			elif item == 'n' and tempvar <= 0:
-					KNIGHTS[index] = 3
-					UNIONNOK[index] = 3
-					UNIONK[index] = 3
-			elif item == 'N' and tempvar <= 0:
-					KNIGHTS[index] = -3
-					UNIONNOK[index] = -3
-					UNIONK[index] = -3
-			elif item == 'b' and tempvar <= 0:
-					BISHOPS[index] = 4
-					UNIONNOK[index] = 4
-					UNIONK[index] = 4
-			elif item == 'B' and tempvar <= 0:
-					BISHOPS[index] = -4
-					UNIONNOK[index] = -4
-					UNIONK[index] = -4
-			elif item == 'p' and tempvar <= 0:
-					PAWNS[index] = 1
-					UNIONNOK[index] = 1
-					UNIONK[index] = 1
-			elif item == 'P' and tempvar <= 0:
-					PAWNS[index] = -1
-					UNIONNOK[index] = -1
-					UNIONK[index] = -1
-			elif item == 'k' and tempvar <= 0:
-					KINGS[index] = 127
-					UNIONK[index] = 127
-			elif item == 'K' and tempvar <= 0:
-					KINGS[index] = -127 
-					UNIONK[index] = -127
-			elif item == 'q' and tempvar <= 0:
-					QUEENS[index] = 9
-					UNIONNOK[index] = 9
-					UNIONK[index] = 9
-			elif item == 'Q' and tempvar <= 0:
-					QUEENS[index] = -9
-					UNIONNOK[index] = -9
-					UNIONK[index] = -9
-			elif isnumeric(item) == True:
-					for v in range(int(item)+1):
-							PAWNS[index+v-1] = 0
-							QUEENS[index+v-1] = 0
-							KINGS[index+v-1] = 0
-							BISHOPS[index+v-1] = 0
-							ROOKS[index+v-1] = 0
-							UNIONK[index+v-1] = 0
-							UNIONNOK[index+v-1] = 0
-							tempvar = item
-      if tempvar < 0:
-        tempvar = tempvar+1
-def findLegal():
-	for i in PAWNS :
-		if  PAWNS[i] == 1 :
-			if turn == 'w' :
-				if i+8 < 64 :
-					legalMoves.append(('Pawn',(i-8)//8,(i-8) % 8))
-				if (i>7 and i<16) or (i>47 and i<56): 
-						legalMoves.append(('Pawn',(i-16)//8,(i-16) % 8))
-				for j in UNIONNOK:
-						if UNIONNOK[j-9] > 0:
-							legalMoves.append(('Pawn',(j-9)//8,(j-9) % 8))
-						if UNIONNOK[j-7] > 0:
-							legalMoves.append(('Pawn',(j-7)//8,(j-7) % 8))
-				for z in KINGS:
-						if KINGS[z-9] > 0:
-							if turn == 'w' and KINGS[z-9] == 127 :
-								legalMoves.append('ILLEGAL POSITION')
-							if turn == 'b' and KINGS[z-9] == -127 :
-								legalMoves.append('ILLEGAL POSITION')
-							if turn == 'w' and KINGS[z-9] == -127 :
-								legalMoves.append('CHECK')
-							if turn == 'b' and KINGS[z-9] == 127 :
-								legalMoves.append('CHECK')
-				for z in KINGS:
-						if KINGS[z-7] > 0 and turn == 'w':
-							if KINGS[z-7] == 127:
-										legalMoves.append('ILLEGAL POSITION')
-							if turn == 'b' and KINGS[z-7] == -127 :
-									legalMoves.append('ILLEGAL POSITION')
-							if turn == 'w' and KINGS[z-7] == -127 :
-										legalMoves.append('CHECK')
-							if turn == 'b' and KINGS[z-7] == 127 :
-										legalMoves.append('CHECK')      
-			elif turn ==  'b' :
-				if i+8 < 64 :
-					legalMoves.append(('Pawn',(i-8)//8,(i-8) % 8))
-				if (i>7 and i<16) or (i>47 and i<56): 
-						legalMoves.append(('Pawn',(i-16)//8,(i-16) % 8))
-				for j in UNIONNOK:
-						if UNIONNOK[j-9] < 0:
-							legalMoves.append(('Pawn',(j-9)//8,(j-9) % 8))
-						if UNIONNOK[j-7] < 0:
-							legalMoves.append(('Pawn',(j-7)//8,(j-7) % 8))
-				for z in KINGS:
-						if KINGS[z-9] < 0:
-							if turn == 'w' and KINGS[z-9] == 127 :
-								legalMoves.append('ILLEGAL POSITION')
-							if turn == 'b' and KINGS[z-9] == -127 :
-								legalMoves.append('ILLEGAL POSITION')
-							if turn == 'w' and KINGS[z-9] == -127 :
-								legalMoves.append('CHECK')
-							if turn == 'b' and KINGS[z-9] == 127 :
-								legalMoves.append('CHECK')
-				for z in KINGS:
-						if KINGS[z-7] < 0 and turn == 'w':
-							if KINGS[z-7] == 127:
-										legalMoves.append('ILLEGAL POSITION')
-							if turn == 'b' and KINGS[z-7] == -127 :
-									legalMoves.append('ILLEGAL POSITION')
-							if turn == 'w' and KINGS[z-7] == -127 :
-										legalMoves.append('CHECK')
-							if turn == 'b' and KINGS[z-7] == 127 :
-										legalMoves.append('CHECK')
-def evaluate():
-		# Finding all legal moves
+tempvar = 0
+eval = 0
+modified_string = ''.join([char for char in FEN])
+import re
+import socket
+import os
 
-		pass
+full_path = os.path.realpath(__file__)
+print(full_path + "\n")
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('', int(os.environ['PORT'])))
+
+s.listen(1)
+
+c, addr = s.accept()
+def conversion():
+  global modified_string
+  for char in modified_string:
+      FENCHANGE.append(char)
+  modified_string= re.sub(r'(\d+)', lambda match: 'l' * int(match.group()), modified_string)
+  modified_string = modified_string.replace('/', '')
+  FENCHANGE.clear()
+  for char in modified_string:
+    if not char.isnumeric() and char != 'l':
+      FENCHANGE.append(char)
+    elif char == 'l':
+      FENCHANGE.append(0)
+  for index, item in enumerate(FENCHANGE):
+        if item == 'r':
+            ROOKS[index] = -5
+            UNIONNOK[index] = -5
+            UNIONK[index] = -5
+        elif item == 'R':
+            ROOKS[index] = 5
+            UNIONNOK[index] = 5
+            UNIONK[index] = 5
+        elif item == 'n':
+            KNIGHTS[index] = -3
+            UNIONNOK[index] = -3
+            UNIONK[index] = -3
+        elif item == 'N':
+            KNIGHTS[index] = 3
+            UNIONNOK[index] = 3
+            UNIONK[index] = 3
+        elif item == 'b':
+            BISHOPS[index] = -4
+            UNIONNOK[index] = -4
+            UNIONK[index] = -4
+        elif item == 'B':
+            BISHOPS[index] = 4
+            UNIONNOK[index] = 4
+            UNIONK[index] = 4
+        elif item == 'p':
+            PAWNS[index] = -1
+            UNIONNOK[index] = -1
+            UNIONK[index] = -1
+        elif item == 'P':
+            PAWNS[index] = 1
+            UNIONNOK[index] = 1
+            UNIONK[index] = 1
+        elif item == 'k':
+            KINGS[index] = -127
+            UNIONK[index] = -127
+        elif item == 'K':
+            KINGS[index] = 127 
+            UNIONK[index] = 127
+        elif item == 'q':
+            QUEENS[index] = -9
+            UNIONNOK[index] = -9
+            UNIONK[index] = -9
+        elif item == 'Q':
+            QUEENS[index] = 9
+            UNIONNOK[index] = 9
+            UNIONK[index] = 9
+
+
+def empty_black(squarenum):
+  if UNIONK[squarenum] < 0:
+    return True
+  else:
+    return False
+def empty_white(squarenum):
+  if UNIONK[squarenum] > 0:
+    return True
+  else:
+    return False
+
+def findLegal():
+  for i in PAWNS :
+    if turn == 'w':
+      if i > 0:
+        if empty_black(i+8) == True:
+          legalMoves.append('P'+str(i+8))
+        if i-7 > 0 and empty_black(i+7) == False:
+          legalMoves.append('P'+str(i+8))
+        if i-9 > 0 and empty_black(i+9) == False and i % 8 != 1:
+          legalMoves.append('P'+str(i+9))
+    elif turn == 'b':
+      if i < 0:
+        if empty_white(i+8) == True:
+          legalMoves.append('P'+str(i+8))
+        if i+7 < 64 and empty_white(i+7) == False: 
+          legalMoves.append('p'+str(i+7))
+        if i+9 < 64 and empty_white(i+9) == False and i %8 != 1: 
+          legalMoves.append('p'+str(i+9))
+
+
+def evaluate():
+    for u in range(len(UNIONNOK)):
+      global eval
+      eval = eval + UNIONNOK[u]
 conversion() 
 findLegal() 
-for x in range(len(UNIONNOK)):
-	print(UNIONNOK[x])
+evaluate()
+# print(eval)
+# print(legalMoves)
+
+print("CONNECTION FROM: ", str(addr))
--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from flask import Flask, render_template, request
 import os
+import socket
 
 full_path = os.path.realpath(__file__)
 print(full_path + "\n")
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('', int(os.environ['PORT'])))
+
+s.listen(1)
+
+c, addr = s.accept()
 
 app = Flask(__name__, template_folder='front_end', static_folder='static')
 
@@ -18,9 +26,9 @@ def analyzer_page():
 @app.route('/', methods=["POST"])
 def index_post():
 	FEN = request.form['FEN']
+	c.send(FEN.encode())
 	return render_template('analyzer_page.html', fen=FEN)
 
 if __name__ == '__main__':
 		# Run the Flask app
 		app.run(host='0.0.0.0', debug=True, port=int(os.environ['PORT']))
-
