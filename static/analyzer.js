10d8a188e76914ee7a767e44855f6430f2b44638
TheAwesomeKid02 <69116138+TheAwesomeKid02@users.noreply.github.com>
2024-02-28 22:32:10 +0000

    Finally got the chessboard to load the pieces correctly

--- static/analyzer.js
+++ static/analyzer.js
@@ -2,71 +2,6 @@ let center = document.createElement('center');
 
 let num = 1;
 
-let a1 = document.getElementById('1');
-let a2 = document.getElementById('2');
-let a3 = document.getElementById('3');
-let a4 = document.getElementById('4');
-let a5 = document.getElementById('5');
-let a6 = document.getElementById('6');
-let a7 = document.getElementById('7');
-let a8 = document.getElementById('8');
-let b1 = document.getElementById('9');
-let b2 = document.getElementById('10');
-let b3 = document.getElementById('11');
-let b4 = document.getElementById('12');
-let b5 = document.getElementById('13');
-let b6 = document.getElementById('14');
-let b7 = document.getElementById('15');
-let b8 = document.getElementById('16');
-let c1 = document.getElementById('17');
-let c2 = document.getElementById('18');
-let c3 = document.getElementById('19');
-let c4 = document.getElementById('20');
-let c5 = document.getElementById('21');
-let c6 = document.getElementById('22');
-let c7 = document.getElementById('23');
-let c8 = document.getElementById('24');
-let d1 = document.getElementById('25');
-let d2 = document.getElementById('26');
-let d3 = document.getElementById('27');
-let d4 = document.getElementById('28');
-let d5 = document.getElementById('29');
-let d6 = document.getElementById('30');
-let d7 = document.getElementById('31');
-let d8 = document.getElementById('32');
-let e1 = document.getElementById('33');
-let e2 = document.getElementById('34');
-let e3 = document.getElementById('35');
-let e4 = document.getElementById('36');
-let e5 = document.getElementById('37');
-let e6 = document.getElementById('38');
-let e7 = document.getElementById('39');
-let e8 = document.getElementById('40');
-let f1 = document.getElementById('41');
-let f2 = document.getElementById('42');
-let f3 = document.getElementById('43');
-let f4 = document.getElementById('44');
-let f5 = document.getElementById('45');
-let f6 = document.getElementById('46');
-let f7 = document.getElementById('47');
-let f8 = document.getElementById('48');
-let g1 = document.getElementById('49');
-let g2 = document.getElementById('50');
-let g3 = document.getElementById('51');
-let g4 = document.getElementById('52');
-let g5 = document.getElementById('53');
-let g6 = document.getElementById('54');
-let g7 = document.getElementById('55');
-let g8 = document.getElementById('56');
-let h1 = document.getElementById('57');
-let h2 = document.getElementById('58');
-let h3 = document.getElementById('59');
-let h4 = document.getElementById('60');
-let h5 = document.getElementById('61');
-let h6 = document.getElementById('62');
-let h7 = document.getElementById('63');
-let h8 = document.getElementById('64');
-
 // Create a table element 
 let ChessTable = document.createElement('table'); 
 for (let i = 0; i < 8; i++) { 
@@ -98,50 +33,59 @@ for (let i = 0; i < 8; i++) {
 		// Append the row 
 		ChessTable.appendChild(board); 
 } 
-center.appendChild(ChessTable); 
+center.appendChild(ChessTable);
 
 // Modifying table attribute properties 
 ChessTable.setAttribute('cellspacing', '0'); 
 ChessTable.setAttribute('width', '675px'); 
 document.body.appendChild(center);
 
-function putPiece(color, piece, square) {
-	let chessmen = document.createElement('img');
+function addPiece(color, piece, square) {
+	let squares = document.querySelectorAll('td');
+	const chessmen = document.createElement('img');
 	chessmen.setAttribute('src', `/static/images/${color}/${piece}.png`);
-		
+	chessmen.style.height = '100%';
+	chessmen.style.width = '100%';
+	squares[square].appendChild(chessmen);
 }
 
 function initialize() {
-		const squares = document.querySelectorAll('td');
-		for (let i = 0; i < 8; i++) {
-			const pawn = document.createElement('img');
-			pawn.setAttribute('src', '/static/images/black/pawn.png');
-			pawn.style.position = 'absolute';
-			pawn.style.width = '100%';
-			pawn.style.height = '100%';
+	let squares = document.querySelectorAll('td');
+	for(let i = 8; i < 16; i++) {
+		const pawn = document.createElement('img');
+		pawn.setAttribute('src', '/static/images/black/pawn.png');
+		pawn.style.height = '100%';
+		pawn.style.width = '100%';
+		squares[i].appendChild(pawn);
+	}
+	for(let i = 48; i < 56; i++) {
+		const pawn = document.createElement('img');
+		pawn.setAttribute('src', '/static/images/white/pawn.png');
+		pawn.style.height = '100%';
+		pawn.style.width = '100%';
+		squares[i].appendChild(pawn);
+	}
 
-			const squareRect = squares[i].getBoundingClientRect(); 
-			const squareCenterX = squareRect.left;
-			const squareCenterY = squareRect.top;
-			pawn.style.top = `${squareCenterY/2+10}px`; // Set the top position of the pawn to the y-coordinate of the center of the square
-			pawn.style.left = `0px`; // Set the left position of the pawn to the x-coordinate of the center of the square
-			squares[i].appendChild(pawn);
-		}
-		for (let i = 32; i < 40; i++) {
-			const pawn = document.createElement('img');
-			pawn.setAttribute('src', '/static/images/white/pawn.png');
-			pawn.style.position = 'absolute';
-			pawn.style.width = '100%';
-			pawn.style.height = '100%';
+	//append the black pieces
+	addPiece('black', 'rook', 0);
+	addPiece('black', 'knight', 1);
+	addPiece('black', 'knight', 6);
+	addPiece('black', 'bishop', 2);
+	addPiece('black', 'bishop', 5);
+	addPiece('black', 'king', 4);
+	addPiece('black', 'queen', 3);
+	addPiece('black', 'rook', 7);
 
-			const squareRect = squares[i].getBoundingClientRect(); 
-			const squareCenterX = squareRect.left;
-			const squareCenterY = squareRect.top;
-			pawn.style.top = `${squareCenterY/2+10}px`; // Set the top position of the pawn to the y-coordinate of the center of the square
-			pawn.style.left = `0px`; // Set the left position of the pawn to the x-coordinate of the center of the square
-			squares[i].appendChild(pawn);
-		}
+
+	//append the white pieces
+	addPiece('white', 'rook', 56);
+	addPiece('white', 'knight', 57);
+	addPiece('white', 'knight', 62);
+	addPiece('white', 'bishop', 58);
+	addPiece('white', 'bishop', 61);
+	addPiece('white', 'king', 60);
+	addPiece('white', 'queen', 59);
+	addPiece('white', 'rook', 63);
 }
-
 
 initialize();
