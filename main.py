06b6f1c9915c24526eab4aa0261c6be7a0fe22f6
TheAwesomeKid02 <69116138+TheAwesomeKid02@users.noreply.github.com>
2024-03-15 22:04:59 +0000

    did last minute annotations

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ full_path = os.path.realpath(__file__)
 print(full_path + "\n")
 app = Flask(__name__, template_folder='front_end', static_folder='static')
 
-# Index page
+# THE FOLLOWING JUST ROUTE DIFFERENT TYPES OF REQUESTS TO THE RESPECTIVE PAGES
 @app.route('/', methods=["GET"])
 def index():
 	return render_template('index.html')
@@ -21,6 +21,7 @@ def analyzer_page():
 
 @app.route('/', methods=["POST"])
 def index_post():
+	#initialize the variables for analysis
 	FEN = request.form['FEN']
 	FEN_new = re.sub(r'\s.*', '', FEN)
 
@@ -30,8 +31,10 @@ def index_post():
 	eval = analysis.evaluate(FEN_new)
 	modified_string = ''.join([char for char in FEN])
 
+	#export the variables to the html page
 	return render_template('analyzer.html', fen=FEN_new, evaluation=eval, pos=position)
 
+#run the app on the port
 if __name__ == '__main__':
 		# Run the Flask app
 		app.run(host='0.0.0.0', debug=True, port=int(os.environ['PORT']))
